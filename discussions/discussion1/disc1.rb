962e3556d0ac2c4dcd2e581fd90034355b65bc68
Hyperian1 <hyperiafire@gmail.com>
2022-09-12 19:06:38 -0400

    true update

--- discussions/discussion1/disc1.rb
+++ discussions/discussion1/disc1.rb
@@ -1,13 +1,20 @@
 class Rectangle
-    def initialize(length, width)
+    @@numRect = 0
 
+    def initialize(length, width)
+        @length = length
+        @width = width
+        @@numRect += 1
     end
 
     def getArea()
-        
+        @length * @width
     end
 
     def self.getNumRectangles()
         
     end
 end
+
+
+
